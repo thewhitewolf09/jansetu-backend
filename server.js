71bcb750271f033a6529b2bd1d20b523b1c3ddca
Sumit Nirmal <sumitramprakashnirmal@gmail.com>
2025-09-18 14:10:01 +0530

    few changes

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 
 
-app.use(cors({ origin: "https://jansetu-portal.vercel.app" }));
+app.use(cors({ origin: ["http://localhost:5173","https://jansetu-portal.vercel.app"] }));
 
 
 
